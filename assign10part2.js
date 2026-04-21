4a207a324c2cfa5939c2ba978e9e64461efa160f
Kavin Kang <kavin.kang@tufts.edu>
2026-04-21 14:23:16 -0400

    With extra credit support
    This will now display the town and zip codes on the page.

--- assign10part2.js
+++ assign10part2.js
@@ -4,20 +4,24 @@ var port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://kavin:12345@cluster0.wi2yeyj.mongodb.net/?appName=Cluster0";
 
-http.createServer(function (req, res) {
+//This will be used to display the town and zip codes on the page
+var printPlace = "NOTHING";
+var printZips = "NOTHING";
+
+http.createServer(async function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var urlObj = url.parse(req.url, true);
-    res.write("Testing this out");
+    res.write("<h1>Kavin Kang Assignment 10 Part 2</h1>");
     path = urlObj.pathname;
     if (path == '/home') {
-        res.write("<h1>HOME</h1>");
+        res.write("<h2>Home</h2>");
         res.write("<form id='placeForm' method='get' action='/process'>");
         res.write("<label>Place</label>: <input type='text' name='userInput'><br/>");
         res.write("<input type='submit' value='Submit'>");
         res.write("</form>");
     }
     else if (path == "/process") {
-        res.write("<h1>PROCESS</h1>");
+        res.write("<h2>Process</h2>");
         //Check to see if it will get the information back
         placeInput = urlObj.query.userInput;
         res.write("<h2>Town or Zip: " + placeInput + "</h2>");
@@ -34,20 +38,39 @@ http.createServer(function (req, res) {
             //Get the town associated with the zip Code
             //Perform a regex to get the place associated with the zip code
             const results = useColl.find({zips: {$regex: placeInput}});
-            results.forEach(function(item) {
+            validRes = false;
+            await results.forEach(function(item) {
                 console.log("Place: " + item.place);
                 console.log("Zips: " + item.zips);
+                printPlace = item.place;
+                printZips = item.zips;
+                validRes = true;
             });
+            if (!validRes) {
+                printPlace = "INVALID";
+                printZips = "INVALID";
+            }
+            res.write("<h3>Place: " + printPlace + "</h3>");
+            res.write("<h3>Zips: " + printZips + "</h3>");
         }
         else {
             res.write("<h3>This is a Town Name</h3>");
             const results = useColl.find({place: placeInput});
-            results.forEach(function(item) {
+            
+            validRes = false;
+            await results.forEach(function(item) {
                 console.log("Place: " + item.place);
                 console.log("Zips: " + item.zips);
-                res.write("<h3>Place: " + item.place + "</h3>");
-                res.write("<h3>Zips: " + item.zips + "</h3>");
+                printPlace = item.place;
+                printZips = item.zips;
+                validRes = true;
             });
+            if (!validRes) {
+                printPlace = "INVALID";
+                printZips = "INVALID";
+            }
+            res.write("<h3>Place: " + printPlace + "</h3>");
+            res.write("<h3>Zips: " + printZips + "</h3>");
         }
     }
     res.end();
